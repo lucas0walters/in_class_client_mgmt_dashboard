8277f1dcd2ce3e03e3b4954501eca10ff30e1550
lucas0walters <lucas.o.walters@gmail.com>
2025-05-17 16:50:56 -0400

    Typo in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'in_class_client_mgmt_dashboard'
+  base: '/in_class_client_mgmt_dashboard'
 })
